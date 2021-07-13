a4e0d914acdca9501e28ff5625add1933a5520aa
Fikri Ali Nawawi <fikrialicakep@gmail.com>
2021-07-13 15:23:41 +0700

    Update 1.select-basics.sql

--- Default/1.select-basics.sql
+++ Default/1.select-basics.sql
@@ -1,1 +1,14 @@
+# Modify it to show the population of Germany
 
+SELECT population FROM world
+  WHERE name = 'Germany'
+  
+# Show the name and the population for 'Sweden', 'Norway' and 'Denmark'
+
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+# Which countries are not too small and not too big? range area from 200000 to 250000
+
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
